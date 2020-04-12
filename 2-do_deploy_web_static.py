5f81032cca6a6118e92a1bad6377e07a63506817
Jdpa357 <jdpa352@hotmail.es>
2020-04-12 23:14:14 +0000

    Adding changes to file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,54 +1,30 @@
 #!/usr/bin/python3
-# This fabscript deploys the web static content
-from os import path
-from fabric.api import run, env, put
+"""Fabric script that distributes an archive to my web servers."""
+from fabric.api import local, run, env, put
+from datetime import datetime
+import os
 
-env.hosts = ["54.158.45.251", "100.24.7.183"]
+env.user = 'ubuntu'
+env.hosts = ['54.160.217.25', '3.87.244.249']
 
 
 def do_deploy(archive_path):
-    """
-    send and configure the web static content
-    """
-    # Assure that given parameter is a file
-    if not path.isfile(archive_path):
+    """Distributes archive to my web servers."""
+    if os.path.isfile(archive_path) is False:
         return False
-
-    # Paths variables
-    file_ = archive_path.split("/")[-1]
-    name = file_.split(".")[0]
-    tmp = "/tmp/{}".format(file_)
-    data = "/data/web_static/releases/{}/".format(name)
-    current = "/data/web_static/current"
-
-    # Send compressed file
-    if put(archive_path, tmp).failed:
+    try:
+        f_name = archive_path.split("/")[-1]
+        no_ext = f_name.split(".")[0]
+        path_no_ext = "/data/web_static/releases/{}/".format(no_ext)
+        symlink = "/data/web_static/current"
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(path_no_ext))
+        run("tar -xzf /tmp/{} -C {}".format(f_name, path_no_ext))
+        run("rm /tmp/{}".format(f_name))
+        run("mv {}web_static/* {}".format(path_no_ext, path_no_ext))
+        run("rm -rf {}web_static".format(path_no_ext))
+        run("rm -rf {}".format(symlink))
+        run("ln -s {} {}".format(path_no_ext, symlink))
+        return True
+    except:
         return False
-    # Remove a previous release
-    if run("rm -rf {}".format(data)).failed:
-        return False
-    # Create a directorty to allocate the new release
-    if run("mkdir -p {}".format(data)).failed:
-        return False
-    # Uncompress archive
-    if run("tar -xzf {} -C {}".format(tmp, data)).failed:
-        return False
-    # Remove compressed archive
-    if run("rm {}".format(tmp)).failed:
-        return False
-    # Move uncomprresed data of web static to the release folder
-    if run("mv {}web_static/* {}".format(data, data)).failed:
-        return False
-    # Remove uncompressed data from sistem
-    if run("rm -rf {}web_static".format(data)).failed:
-        return False
-    # Remove previous link to web static content
-    if run("rm -rf {}".format(current)).failed:
-        return False
-    # Create a new link to the new release content
-    if run("ln -s {} {}".format(data, current)).failed:
-        return False
-    # Finish the deploy!
-    print("New version deployed!")
-
-    return True

c97bb8215cb0cb9cf29753dd99f09b41e941100b
Jdpa357 <jdpa352@hotmail.es>
2020-04-13 03:42:34 +0000

    Adding super user command to file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,13 +18,13 @@ def do_deploy(archive_path):
         path_no_ext = "/data/web_static/releases/{}/".format(no_ext)
         symlink = "/data/web_static/current"
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(path_no_ext))
-        run("tar -xzf /tmp/{} -C {}".format(f_name, path_no_ext))
-        run("rm /tmp/{}".format(f_name))
-        run("mv {}web_static/* {}".format(path_no_ext, path_no_ext))
-        run("rm -rf {}web_static".format(path_no_ext))
-        run("rm -rf {}".format(symlink))
-        run("ln -s {} {}".format(path_no_ext, symlink))
+        run("sudo mkdir -p {}".format(path_no_ext))
+        run("sudo tar -xzf /tmp/{} -C {}".format(f_name, path_no_ext))
+        run("sudo rm /tmp/{}".format(f_name))
+        run("sudo mv {}web_static/* {}".format(path_no_ext, path_no_ext))
+        run("sudo rm -rf {}web_static".format(path_no_ext))
+        run("sudo rm -rf {}".format(symlink))
+        run("sudo ln -s {} {}".format(path_no_ext, symlink))
         return True
     except:
         return False
